2cf5eba347d0ff1b095a94fc8a1aac508c962ca2
Arslan038 <arslanahmed983@gmail.com>
2026-07-01 15:42:28 +0500

    updated counter with localStorage

---
+++ src/components/counter/Counter.vue
@@ -0,0 +1,30 @@
+<template>
+	<h1>Counter</h1>
+	<p>Current Count: {{ count }}</p>
+
+	<BaseButton variant="primary" text="Increment" @click="increment" />
+	<BaseButton variant="secondary" text="Decrement" @click="decrement" />
+	<BaseButton variant="warning" text="Reset" @click="reset" />
+</template>
+
+<script setup lang="ts">
+import { onMounted } from 'vue';
+import { useCounter } from '../../composables/useCounter';
+import BaseButton from '../ui/BaseButton.vue';
+
+const { count, increment, decrement, reset } = useCounter();
+
+</script>
+
+<style scoped lang="scss">
+h1 {
+	font-size: 2em;
+	margin-bottom: 10px;
+}
+
+button {
+	margin: 5px;
+	padding: 10px;
+	font-size: 1em;
+}
+</style>
---
+++ src/composables/useCounter.ts
@@ -0,0 +1,42 @@
+import { onMounted, ref } from "vue";
+import { COUNTER_KEY } from "../constants/counter";
+import { safeParse, safeStringify } from "../utils/safeParse";
+
+export function useCounter() {
+    const count = ref(0);
+
+    function increment() {
+        count.value++;
+        localStorage.setItem(COUNTER_KEY, safeStringify(count.value));
+    }
+
+    function decrement() {
+        count.value--;
+        localStorage.setItem(COUNTER_KEY, safeStringify(count.value));
+    }
+
+    function reset() {
+        count.value = 0;
+        localStorage.setItem(COUNTER_KEY, safeStringify(count.value));
+    }
+
+    function getCounter() {
+        const countValue = safeParse(localStorage.getItem(COUNTER_KEY), 0);
+        return countValue;
+    }
+
+    onMounted(() => {
+        const countValue = getCounter();
+        if (countValue) {
+            count.value = countValue;
+        }
+    })
+
+    return {
+        count,
+        increment,
+        decrement,
+        reset,
+        getCounter
+    }
+}
